0d552a40e129cb841442314666bfb51e2a380ddc
Chisama Musonda <Chase.Musonda@gmail.com>
2021-06-25 23:59:34 +0200

    Project completion-Ready for submission

--- upload_docker.sh
+++ upload_docker.sh
@@ -6,10 +6,15 @@
 # Step 1:
 # Create dockerpath
 # dockerpath=<your docker ID/path>
+export dockerpath="chasetheblack/microservices"
 
 # Step 2:  
 # Authenticate & tag
+docker login --username chasetheblack
+docker tag microservices $dockerpath
 echo "Docker ID and Image: $dockerpath"
+
 
 # Step 3:
 # Push image to a docker repository
+docker push $dockerpath
